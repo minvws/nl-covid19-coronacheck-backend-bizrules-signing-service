4fc5ab02975cbbe9b73b283f968b8997d9d461ac
stitch1 <elger.jonker@gmail.com>
2021-05-27 15:18:37 +0200

    add comments

--- api/signers/nl_domestic_dynamic.py
+++ api/signers/nl_domestic_dynamic.py
@@ -3,7 +3,7 @@
 
 from typing import List, Optional, Any
 
-from api.models import DomesticStaticQrResponse, StepTwoData, DomesticGreenCard
+from api.models import StepTwoData, DomesticGreenCard
 from api.settings import settings
 from api.signers.nl_domestic_static import vaccination_event_data_to_signing_data
 from api.utils import request_post_with_retries
@@ -25,38 +25,13 @@ def sign(data: StepTwoData, prepare_issue_message: Any) -> Optional[DomesticGree
 
     Todo: it's unclear if there will be one or more requests, probably covering 10 days of 24 hour codes.
     Todo: implement this for multiple days. How many, is that a param / config variable?
-    {
-        "attributes": {
-            # difference between static and dynamic
-            "sampleTime": "2021-04-20T03:00:00Z",
-            "firstNameInitial": "B",
-            "lastNameInitial": "B",
-            "birthDay": "28",
-            "birthMonth": "4",
-            "isSpecimen": True
-        },
-        "nonce": "MB4CEEl6phUEfUcOWLWHEHQ6/zYTClZXUy1URVNULTA=",
-        "commitments": "eyJuXzIiOiI4YTdqYlB1MTRJSUNZLzVqUmtrRExBPT0iLCJjb21iaW5lZFByb29mcyI6W3siVSI6ImR5bVQvWnNmZzd"
-                       "6VUxyZDFGMjVFR3pjdUgyM2RqWmYwcnlvcjhRQjM1NXA1L1FmdFhPanNkc1dTdFZHcUFkUHRUaVg1NjRISGlSR0NpeU"
-                       "1pZ2FWUUlGZ3FZSk1YS2JaMituekJpM09rL2RRZDl3dDIwWFIvd2JtSE1JN2kzMkJQN0FXZWRoZ3paT0dOUlBsQTQrW"
-                       "mFvTGVHVVVJT2M2TlIreldoRW9PUFFaSmZrcjd1bm0wL3BjR0IvOHBPNHl3NHNYQnZsajVucU96b0dIbHhscEYxWTF0"
-                       "azFJQWJWd0FkN2lDYzJjNVlXNzFlYzZOeEt1SDJyU1ltVTgvaWg4d3M5dGk0elpKT3JUQ0NlMHNwaEpwOE1ZRzNkRmZ"
-                       "ybjF6WGtzV3l2UnhXRG5XMW1pbE9pMWZ2UlVFZmtkeW1vcFRRZUhaVFEvS0xUYjNmNUkzWnNpUXpKUT09IiwiYyI6Im"
-                       "J1b2tFYXZxNkY0czNyYklqZVJ0b0t3bWRPVUptc3hnUEtuT2xic3ZsQmc9Iiwidl9wcmltZV9yZXNwb25zZSI6Ilp6S"
-                       "FNXWWVTTEtpMWZRUnlpNTFnSUxZWDVXeENlQmppTmwxZjVaNlFrUTVqMzZKTnNKd3Y0diszMDI0Z1VvaFA3Ry9sOFRa"
-                       "cnFEVTdEa1kvdE51N1RUM2xQeU8yMEdhWkNQZE9tUVFmTVovSDdoUGZFRHZnOE9MbkZuK0JZWGgrZkEyVHpBQ1c1SDl"
-                       "aTTJpK1NCZEdZa29wbzRlT0NTVC8wWUZhbndxS05GQUNqMVVWL2dTcEREbHBRS05iN2R5czFzUjZBS01MWEsxVjhlUX"
-                       "JGU3JSTXVXaTgzLzNHVjhRT2ZFMkd2VUpsMzYrdTVPcmdxOFdJWnVJUHhuRnRtdTZTQ0lFNWJnMVZEVllxU3ZGdEoyU"
-                       "TRxaU0rOWkwa2VDNyt3ZENKM1k0b1QyNldIMHVSeDg0d2tJUXI5aXVSVkY4RU1xQUgwQngyYmorcXlXc3VQbDJRRDJl"
-                       "UWw4b3pyZFVULzJiUHVOYlFrZ0dVUVNPK3FxWlFlUHZHd3k4eThMODVHaWN3YXA0eWQremNRaE9QeDBVWG91SHFWYW9"
-                       "EbE5GQ1VMU1hlaXVoR0U9Iiwic19yZXNwb25zZSI6InlLNjMyOWVXcUhJNVA0QUc0T0FmdUZMa0Q1YytqeUlYVFQwbW"
-                       "hvTEE5UU93b0xyVjBvdW4wMFZRaFBoazA0TUw2NUhFcmI2NlkxMmZ6QzFJTndLNTNnaWlBeXpnMnU3TDFYcz0ifV19",
-        "key": "VWS-TEST-0"
-    }
 
     :param data:
     :return:
     """
+
+    # todo: afhankelijk van regels moeten we bepalen welke origins en welke sample times er zijn.
+    # events reduceren tot origins met sampletimes.
 
     # https://github.com/minvws/nl-covid19-coronacheck-idemix-private/blob/next/issuer/issuer.go
     # en de verschillende origins die bedenken we zelf maar sturen we niet mee in de attributes.
