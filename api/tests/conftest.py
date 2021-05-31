25e202a1abbfeb1dc084e4dd590a0b98edb88f01
Maarten Derickx <maarten.derickx@navara.nl>
2021-05-31 16:33:43 +0200

    make valid

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,17 +1,17 @@
 # allow access of private variables for mocking purposes
 # pylint: disable=W0212
 import pytest
-from pytest_redis.factories import redis_noproc
 
 from api.constants import INGE4_ROOT, TESTS_DIR
-from api.settings import settings
 from api.session_store import session_store
+from api.settings import settings
 
 if settings.USE_PYTEST_REDIS:
 
     @pytest.fixture
     def redis_db(redisdb):
         yield redisdb
+
 
 else:
 
