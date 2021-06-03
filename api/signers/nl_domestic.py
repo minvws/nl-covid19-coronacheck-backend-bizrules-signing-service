bd003328cb30aadcf9520a5bff8990be735f3c0b
stitch1 <elger.jonker@gmail.com>
2021-06-03 18:12:02 +0200

    abstract signers, todo: attributes are missing, fix tests

---
+++ api/signers/nl_domestic.py
@@ -0,0 +1,269 @@
+import base64
+import secrets
+from datetime import date, datetime, timedelta
+from typing import List, Optional, Tuple, Union
+
+import pytz
+
+from api import log
+from api.models import (
+    ContiguousOriginsBlock,
+    DomesticGreenCard,
+    DomesticSignerAttributes,
+    Event,
+    Events,
+    EventType,
+    GreenCardOrigin,
+    RichOrigin,
+    StripType,
+)
+from api.settings import settings
+from api.signers import hpkcodes
+from api.utils import request_post_with_retries
+
+ALLOWED_POSITIVE_TEST_TYPES = ["LP217198-3", "LP6464-4"]
+TZ = pytz.timezone("UTC")
+
+
+def floor_hours(my_date: Union[datetime, date]) -> datetime:
+    # Datetimes are automatically marshalled to ISO in json.
+    # cast dates to datetimes:
+    # https://stackoverflow.com/questions/1937622/convert-date-to-datetime-in-python/1937636
+    if isinstance(my_date, date):
+        my_date = datetime.combine(my_date, datetime.min.time())
+
+    combined_date = my_date.replace(microsecond=0, second=0, minute=0)
+    return TZ.localize(combined_date)
+
+
+def eligible_vaccination(events: Events) -> List[RichOrigin]:
+    best_vacc: Optional[Event] = None
+
+    if len(events.vaccinations) > 1:
+        # Ignoring the complexities of bad data quality (i.e. two events for one vaccination)
+        best_vacc = events.vaccinations[1]
+
+    if len(events.vaccinations) == 1:
+        vacc = events.vaccinations[0]
+
+        # One vaccination of the right type, medically approved or combined with a recovery
+        is_eligible = (
+            vacc.vaccination.hpkCode == hpkcodes.JANSSEN  # type: ignore
+            or vacc.vaccination.completedByMedicalStatement  # type: ignore
+            or any(map(lambda r: r.holder.equal_to(vacc.holder), events.recoveries))
+            or any(map(lambda p: p.holder.equal_to(vacc.holder), events.positivetests))
+        )
+
+        if is_eligible:
+            best_vacc = vacc
+
+    # If there a valid vaccination was found, add a single one to the origins
+    if best_vacc:
+        event_time = floor_hours(best_vacc.vaccination.date)  # type: ignore
+
+        return [
+            RichOrigin(
+                holder=best_vacc.holder,
+                type=EventType.vaccination,
+                eventTime=event_time,
+                validFrom=event_time,
+                expirationTime=(event_time + timedelta(days=365)),
+            )
+        ]
+
+    return []
+
+
+def eligible_recovery(events) -> List[RichOrigin]:
+    eligible_recs = list(filter(lambda pt: pt.positivetest.type in ALLOWED_POSITIVE_TEST_TYPES, events.positivetests))
+
+    # TODO: Determine if we really want to blindly copy these values, or just use the same
+    #  calculations as a positive test, based on the sampleDate
+    return [
+        RichOrigin(
+            holder=rec.holder,
+            type=EventType.recovery,
+            eventTime=floor_hours(rec.recovery.sampleDate),
+            validFrom=floor_hours(rec.recovery.validFrom),
+            expirationTime=floor_hours(rec.recovery.validUntil),
+        )
+        for rec in eligible_recs
+    ]
+
+
+def eligible_positive_tests(events) -> List[RichOrigin]:
+    eligible_pts = list(filter(lambda pt: pt.positivetest.type in ALLOWED_POSITIVE_TEST_TYPES, events.positivetests))
+
+    origins = []
+
+    for positive_test in eligible_pts:
+        event_time = floor_hours(positive_test.positivetest.sampleDate)
+
+        origins.append(
+            RichOrigin(
+                holder=positive_test.holder,
+                type=EventType.recovery,
+                eventTime=event_time,
+                validFrom=event_time + timedelta(days=11),
+                expirationTime=event_time + timedelta(days=11 + 180),
+            )
+        )
+
+    return origins
+
+
+def eligible_negative_tests(events) -> List[RichOrigin]:
+    eligible_nts = list(filter(lambda _: True, events.negativetests))
+
+    origins = []
+
+    for negative_test in eligible_nts:
+        event_time = floor_hours(negative_test.negativetest.sampleDate)
+
+        origins.append(
+            RichOrigin(
+                holder=negative_test.holder,
+                # todo: temporarily only return test, as that is easier for the app devs.
+                type=EventType.test,
+                eventTime=event_time,
+                validFrom=event_time,
+                expirationTime=event_time + timedelta(hours=40),
+            )
+        )
+
+    return origins
+
+
+def calculate_attributes_from_blocks(contiguous_blocks: List[ContiguousOriginsBlock]) -> List[DomesticSignerAttributes]:
+    # # Calculate sets of credentials for every block
+    # todo: visualize what is meant with blocks. Add examples.
+    rounded_now = floor_hours(datetime.now())
+
+    attributes = []
+
+    # Calculate the maximum expiration time we're going to issue credentials for
+    maximum_expiration_time = rounded_now + timedelta(days=settings.DOMESTIC_MAXIMUM_ISSUANCE_DAYS)
+
+    for overlapping_block in contiguous_blocks:
+        # Initialize the scrubber with time that is valid and not in the past
+        expiration_time_scrubber = max(rounded_now, overlapping_block.validFrom)
+
+        while True:
+            # Decide on a random number of hours that the current credential will overlap
+            rand_overlap_hours = secrets.randbelow(settings.DOMESTIC_MAXIMUM_RANDOMIZED_OVERLAP_HOURS + 1)
+
+            # Calculate the expiry time for this credential, considering the validity and random overlap,
+            #  while it shouldn't be higher than the expiry time of this contiguous block
+            expiration_time_scrubber += timedelta(hours=settings.DOMESTIC_STRIP_VALIDITY_HOURS) - timedelta(
+                hours=rand_overlap_hours
+            )
+            expiration_time_scrubber = min(expiration_time_scrubber, overlapping_block.expirationTime)
+
+            # Break out if we're past the range we're issuing in
+            if expiration_time_scrubber >= maximum_expiration_time:
+                break
+
+            # Finally add the credential
+            # TODO: Don't use the first holder, but the applicable holder
+            valid_from = expiration_time_scrubber - timedelta(hours=settings.DOMESTIC_STRIP_VALIDITY_HOURS)
+            holder = overlapping_block.origins[0].holder
+
+            domestic_signer_attributes = DomesticSignerAttributes(
+                **{
+                    "isSpecimen": "0",
+                    "stripType": StripType.APP_STRIP,
+                    "validFrom": str(int(valid_from.now().timestamp())),
+                    "validForHours": settings.DOMESTIC_STRIP_VALIDITY_HOURS,
+                    "firstNameInitial": holder.first_name_initial,
+                    "lastNameInitial": holder.last_name_initial,
+                    # Dutch Birthdays can be unknown, supplied as 1970-XX-XX. See DutchBirthDate
+                    "birthDay": str(holder.birthDate.day) if holder.birthDate.day else "",
+                    "birthMonth": str(holder.birthDate.month) if holder.birthDate.month else "",
+                }
+            )
+            domestic_signer_attributes.strike()
+            attributes.append(domestic_signer_attributes)
+
+            # Break out if we're done with this block
+            if expiration_time_scrubber == overlapping_block.expirationTime:
+                break
+
+    log.debug(f"Found {len(attributes)} attributes")
+    return attributes
+
+
+def create_origins(events):
+    origins: List[RichOrigin] = (
+        eligible_vaccination(events)
+        + eligible_recovery(events)
+        + eligible_positive_tests(events)
+        + eligible_negative_tests(events)
+    )
+
+    # # --------------------------------------
+    # # Calculate final origins and attributes
+    # # --------------------------------------
+    # Filter out origins that aren't valid any more, and sort on validFrom
+    rounded_now = floor_hours(datetime.now())
+    return sorted(filter(lambda o: o.expirationTime > rounded_now, origins), key=lambda o: o.validFrom)
+
+
+def create_attributes(origins: List[RichOrigin]) -> List[DomesticSignerAttributes]:
+
+    # # Calculate blocks of contiguous origins
+    contiguous_blocks: List[ContiguousOriginsBlock] = [
+        ContiguousOriginsBlock.from_origin(origins[0]),
+    ]
+
+    for origin in origins[1:]:
+        last_block = contiguous_blocks[-1]
+        if origin.validFrom <= last_block.expirationTime:
+            last_block.origins.append(origin)
+            last_block.expirationTime = max(last_block.expirationTime, origin.expirationTime)
+        else:
+            contiguous_blocks.append(ContiguousOriginsBlock.from_origin(origin))
+
+    log.debug(f"Found {len(contiguous_blocks)} contiguous_blocks.")
+
+    return calculate_attributes_from_blocks(contiguous_blocks)
+
+
+def _sign(url, data, origins) -> DomesticGreenCard:
+    response = request_post_with_retries(
+        url,
+        data=data,
+        headers={"accept": "application/json", "Content-Type": "application/json"},
+    )
+    response.raise_for_status()
+    dcc = DomesticGreenCard(
+        origins=[
+            GreenCardOrigin(
+                type=origin.type,
+                eventTime=origin.eventTime.isoformat(),
+                validFrom=origin.validFrom.isoformat(),
+                expirationTime=origin.expirationTime.isoformat(),
+            )
+            for origin in origins
+        ],
+        createCredentialMessages=base64.b64encode(response.content).decode("UTF-8"),
+    )
+    return dcc
+
+
+def create_origins_and_attributes(
+    events: Events,
+) -> Tuple[bool, Optional[List[RichOrigin]], Optional[List[DomesticSignerAttributes]]]:
+    # todo: add errors why this cant continue.
+
+    # Continue with at least one origin
+    origins = create_origins(events)
+    if len(origins) == 0:
+        return False, None, None
+
+    attributes = create_attributes(origins)
+    if not attributes:
+        # todo: attributes are not extracted yet, something is off. Has to be investigated.
+        log.warning("No relevant attributes, so cannot sign.")
+        # return None
+
+    return True, origins, attributes
--- api/tests/test_sign_via_inge3.py
+++ api/tests/test_sign_via_inge3.py
@@ -1,17 +1,147 @@
 import json
+from datetime import datetime, timezone
+from typing import Dict, List
 
-import pytest
+import json5
+import pytz
 from fastapi.testclient import TestClient
 from freezegun import freeze_time
 
-from api.app import app
-from api.models import DomesticStaticQrResponse, EUGreenCard, PaperProofOfVaccination
+from api.app import app, decode_and_normalize_events
+from api.models import (
+    CMSSignedDataBlob,
+    DomesticSignerAttributes,
+    DomesticStaticQrResponse,
+    EUGreenCard,
+    Event,
+    Events,
+    EventType,
+    Holder,
+    Negativetest,
+    PaperProofOfVaccination,
+    RichOrigin,
+)
 from api.settings import settings
+from api.signers import eu_international
+from api.signers.nl_domestic import create_attributes, create_origins
+from api.utils import read_file
+
+
+def get_testevents(current_path) -> List[CMSSignedDataBlob]:
+    # emulate pydantic, can probably be done better
+    raw_events: List[Dict[str, str]] = json5.loads(read_file(current_path.joinpath("test_data/events1.json5")))
+    return [CMSSignedDataBlob(**event) for event in raw_events]
 
 
 @freeze_time("2020-02-02")
-@pytest.mark.skip(reason="Static signer is up for refactoring.")
-def test_sign_via_inge3(requests_mock):
+def test_static_sign(current_path):
+    events = decode_and_normalize_events(get_testevents(current_path))
+    origins = create_origins(events)
+    assert origins == [
+        RichOrigin(
+            holder=Holder(firstName="Top", lastName="Pertje", birthDate="1950-01-01"),
+            type="test",
+            eventTime=datetime(2021, 5, 27, 0, 0, tzinfo=pytz.utc),
+            validFrom=datetime(2021, 5, 27, 0, 0, tzinfo=pytz.utc),
+            expirationTime=datetime(2021, 5, 28, 16, 0, tzinfo=pytz.utc),
+        ),
+        RichOrigin(
+            holder=Holder(firstName="Top", lastName="Pertje", birthDate="1950-01-01"),
+            type="test",
+            eventTime=datetime(2021, 5, 27, 0, 0, tzinfo=pytz.utc),
+            validFrom=datetime(2021, 5, 27, 0, 0, tzinfo=pytz.utc),
+            expirationTime=datetime(2021, 5, 28, 16, 0, tzinfo=pytz.utc),
+        ),
+        RichOrigin(
+            holder=Holder(firstName="B", lastName="B", birthDate="1883-06-09"),
+            type="test",
+            eventTime=datetime(2021, 6, 1, 0, 0, tzinfo=pytz.utc),
+            validFrom=datetime(2021, 6, 1, 0, 0, tzinfo=pytz.utc),
+            expirationTime=datetime(2021, 6, 2, 16, 0, tzinfo=pytz.utc),
+        ),
+    ]
+
+    attributes: List[DomesticSignerAttributes] = create_attributes(origins)
+    assert attributes == {}
+
+    # b64encode(json.dumps({}).encode()).decode(), b64encode(json.dumps({}).encode()).decode()
+    signed = eu_international.sign(events)
+
+    assert signed == {}
+
+
+@freeze_time("2020-02-02")
+def test_decode_and_normalize_events(current_path):
+    events = decode_and_normalize_events(get_testevents(current_path))
+
+    assert events == Events(
+        events=[
+            Event(
+                type=EventType.negativetest,
+                unique="7ff88e852c9ebd843f4023d148b162e806c9c5fd",
+                isSpecimen=True,
+                negativetest=Negativetest(
+                    sampleDate=datetime(2021, 5, 27, 19, 23, tzinfo=timezone.utc),
+                    resultDate=datetime(2021, 5, 27, 19, 38, tzinfo=timezone.utc),
+                    negativeResult=True,
+                    facility="Facility1",
+                    type="LP6464-4",
+                    name="Test1",
+                    manufacturer="1232",
+                    country="NLD",
+                ),
+                positivetest=None,
+                vaccination=None,
+                recovery=None,
+                source_provider_identifier="ZZZ",
+                holder=Holder(firstName="Top", lastName="Pertje", birthDate="1950-01-01"),
+            ),
+            Event(
+                type=EventType.negativetest,
+                unique="7ff88e852c9ebd843f4023d148b162e806c9c5fd",
+                isSpecimen=True,
+                negativetest=Negativetest(
+                    sampleDate=datetime(2021, 5, 27, 19, 23, tzinfo=timezone.utc),
+                    resultDate=datetime(2021, 5, 27, 19, 38, tzinfo=timezone.utc),
+                    negativeResult=True,
+                    facility="Facility1",
+                    type="LP6464-4",
+                    name="Test1",
+                    manufacturer="1232",
+                    country="NLD",
+                ),
+                positivetest=None,
+                vaccination=None,
+                recovery=None,
+                source_provider_identifier="ZZZ",
+                holder=Holder(firstName="Top", lastName="Pertje", birthDate="1950-01-01"),
+            ),
+            Event(
+                type=EventType.negativetest,
+                unique="19ba0f739ee8b6d98950f1a30e58bcd1996d7b3e",
+                isSpecimen=True,
+                negativetest=Negativetest(
+                    sampleDate=datetime(2021, 6, 1, 5, 40, tzinfo=timezone.utc),
+                    resultDate=datetime(2021, 6, 1, 5, 40, tzinfo=timezone.utc),
+                    negativeResult=True,
+                    facility="not available",
+                    type="LP217198-3",
+                    name="not available",
+                    manufacturer="not available",
+                    country="NLD",
+                ),
+                positivetest=None,
+                vaccination=None,
+                recovery=None,
+                source_provider_identifier="ZZZ",
+                holder=Holder(firstName="B", lastName="B", birthDate="1883-06-09"),
+            ),
+        ]
+    )
+
+
+@freeze_time("2020-02-02")
+def test_sign_via_inge3(requests_mock, current_path):
     signing_response_data = {
         "qr": {
             "data": "TF+*JY+21:6 T%NCQ+ PVHDDP+Z-WQ8-TG/O3NLFLH3:FHS-RIFVQ:UV57K/.:R6+.MX:U$HIQG3FVY%6NIN0:O.KCG9F99",
@@ -40,49 +170,12 @@ def test_sign_via_inge3(requests_mock):
     requests_mock.post(settings.EU_INTERNATIONAL_SIGNING_URL, json=eu_example_answer)
     requests_mock.post("http://testserver/app/paper/", real_http=True)
 
+    events = json5.loads(read_file(current_path.joinpath("test_data/events1.json5")))
+
     client = TestClient(app)
-    response = client.post(
-        "/app/paper/",
-        json={  # pylint: disable=duplicate-code
-            "protocolVersion": "3.0",
-            "providerIdentifier": "XXX",
-            "status": "complete",
-            "holder": {"firstName": "Henk", "lastName": "Akker", "birthDate": "1970-01-01"},
-            "events": [
-                {
-                    "type": "vaccination",
-                    "unique": "ee5afb32-3ef5-4fdf-94e3-e61b752dbed9",
-                    "data": {
-                        "date": "2021-01-01",
-                        "hpkCode": "2924528",
-                        "type": "C19-mRNA",
-                        "manufacturer": "PFIZER",
-                        "brand": "COVID-19 VACCIN PFIZER INJVLST 0,3ML",
-                        "administeringCenter": "",
-                        "country": "NLD",
-                        "doseNumber": 2,
-                        "totalDoses": 2,
-                    },
-                },
-                {
-                    "type": "vaccination",
-                    "unique": "ee5afb32-3ef5-4fdf-94e3-e61b752dbed9",
-                    "data": {
-                        "date": "2021-04-01",
-                        "hpkCode": "2924528",
-                        "type": "C19-mRNA",
-                        "manufacturer": "PFIZER",
-                        "brand": "COVID-19 VACCIN PFIZER INJVLST 0,3ML",
-                        "administeringCenter": "",
-                        "country": "NLD",
-                        "doseNumber": 2,
-                        "totalDoses": 2,
-                    },
-                },
-            ],
-        },
-        headers={},
-    )
+    response = client.post("/app/paper/", json={"events": events}, headers={})
+
+    print(response.text)
 
     signatures: PaperProofOfVaccination = PaperProofOfVaccination(**response.json())
     # 108 QR codes.
