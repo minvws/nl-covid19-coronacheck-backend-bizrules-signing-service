a494aef4b4d74866053a4e61fcda97c496c44a63
stitch1 <elger.jonker@gmail.com>
2021-06-14 17:04:46 +0200

    another attempt to fix redis db test

--- api/tests/test_prepare_issue.py
+++ api/tests/test_prepare_issue.py
@@ -7,11 +7,14 @@ from freezegun import freeze_time
 from api.models import PrepareIssueResponse
 from api.requesters.prepare_issue import get_prepare_issue
 from api.settings import settings
+from api.session_store import session_store
 
 
 @freeze_time("2021-05-20")
 @pytest.mark.asyncio
 async def test_get_prepare_issue(redis_db, requests_mock):
+    # mock redis, disableW0212 since we should be able to access private members for mocking
+    session_store._redis = redis_db  # pylint: disable=W0212
 
     example_response = {"issuerPkId": "TST-KEY-01", "issuerNonce": "kdRNFRIzXiaeYAetJBQdMg==", "credentialAmount": 28}
     requests_mock.post(settings.DOMESTIC_NL_VWS_PREPARE_ISSUE_URL, json=example_response)
