8c170e5f07f36de0cc02ad33702b73b0b3bc05a2
stitch1 <elger.jonker@gmail.com>
2021-06-14 15:12:39 +0200

    add rudimentary test for prepare issue

---
+++ api/tests/test_prepare_issue.py
@@ -0,0 +1,27 @@
+import json
+from base64 import b64decode
+
+import pytest
+from freezegun import freeze_time
+
+from api.models import PrepareIssueResponse
+from api.requesters.prepare_issue import get_prepare_issue
+from api.settings import settings
+
+
+@freeze_time("2021-05-20")
+@pytest.mark.asyncio
+async def test_get_prepare_issue(redisdb, requests_mock):
+
+    example_response = {"issuerPkId": "TST-KEY-01", "issuerNonce": "kdRNFRIzXiaeYAetJBQdMg==", "credentialAmount": 28}
+    requests_mock.post(settings.DOMESTIC_NL_VWS_PREPARE_ISSUE_URL, json=example_response)
+
+    data: PrepareIssueResponse = await get_prepare_issue()
+    print(b64decode(data.prepareIssueMessage))
+    assert (
+        data.prepareIssueMessage == "eyJpc3N1ZXJQa0lkIjogIlRTVC1LRVktMDEiLCAiaXNzdWVyTm9uY2UiOiAia2RSTkZSSXpYaWFlWUF"
+        "ldEpCUWRNZz09IiwgImNyZWRlbnRpYWxBbW91bnQiOiAyOH0="
+    )
+
+    decoded = json.loads(b64decode(data.prepareIssueMessage))
+    assert decoded == {"issuerPkId": "TST-KEY-01", "issuerNonce": "kdRNFRIzXiaeYAetJBQdMg==", "credentialAmount": 28}
