2d45aaab92aa7818ceb0316cb4b2bdd8a7911771
stitch1 <elger.jonker@gmail.com>
2021-06-15 17:05:28 +0200

    add explanation to uci_log

--- api/__init__.py
+++ api/__init__.py
@@ -17,4 +17,5 @@ with open(
 log = logging.getLogger(__package__)
 
 # The UCI logger is used to trace outgoing EU certificates
+# This logger is defined separately in the logging config. We use this for EU compliance reasons.
 uci_log = logging.getLogger("uci")
