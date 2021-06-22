0b0abc47eb582aace9cd0202fe6e42113bd9f669
stitch1 <elger.jonker@gmail.com>
2021-06-22 16:01:45 +0200

    add additional dynamic testcsae

--- api/tests/app_request_tests/test_app_credentials.py
+++ api/tests/app_request_tests/test_app_credentials.py
@@ -76,3 +76,81 @@ def test_app_credential_request(mock_signers, requests_mock, current_path, redis
     }
 
     assert response_data == expected_data
+
+
+@freeze_time("2021-06-22")
+def test_app_credential_request_alt(mock_signers, requests_mock, current_path, redis_db):
+    # Another end to end test.
+    # mock redis, disableW0212 since we should be able to access private members for mocking
+    # # create fake session:
+    session_store._redis = redis_db  # pylint: disable=W0212
+    session_token = session_store.store_message(b64encode(b'{"some": "data"}'))
+
+    requests_mock.post("http://testserver/app/credentials/", real_http=True)
+
+    event = json5.loads("""
+    {
+          "protocolVersion": "3.0",
+          "providerIdentifier": "GGD",
+          "status": "complete",
+          "holder": {
+            "firstName": "Henk",
+            "lastName": "Vries",
+            "infix": null,
+            "birthDate": "1976-10-16"
+          },
+          "events": [
+            {
+              "type": "vaccination",
+              "unique": "0541a651-b49c-43f1-8ba7-15d3f96709ee",
+              "isSpecimen": false,
+              "vaccination": {
+                "date": "2021-06-08",
+                "hpkCode": "2934701",
+                "type": null,
+                "manufacturer": null,
+                "brand": null,
+                "totalDoses": null,
+                "doseNumber": null,
+                "completedByMedicalStatement": null,
+                "completedByPersonalStatement": null
+              }
+            }
+          ]
+    }""")
+
+    credentials_request_data = {
+        "stoken": session_token,
+        "issueCommitmentMessage": b64encode(b'{}').decode("UTF-8"),
+        "events": [{"signature": "", "payload": b64encode(json.dumps(event).encode()).decode("UTF-8")}]
+    }
+
+    client = TestClient(app)
+    response = client.post("/app/credentials/", json=credentials_request_data)
+
+    response_data = response.json()
+
+    assert response_data == {
+        'domesticGreencard': {
+            'createCredentialMessages': 'eyJjcmVkZW50aWFsIjogIkFfUVJfQ09ERSJ9',
+            'origins': [
+                {
+                    'eventTime': '2021-06-08T00:00:00+00:00',
+                    'expirationTime': '2021-12-05T00:00:00+00:00',
+                    'type': 'vaccination',
+                    'validFrom': '2021-06-08T00:00:00+00:00'
+                }
+            ]
+        },
+        'euGreencards': [
+            {'credential': 'A_QR_CODE',
+             'origins': [
+                 {
+                     'eventTime': '2021-06-08T00:00:00+00:00',
+                     'expirationTime': '2021-12-19T00:00:00+00:00',
+                     'type': 'vaccination',
+                     'validFrom': '2021-06-08T00:00:00+00:00'}
+             ]
+             }
+        ]
+    }
